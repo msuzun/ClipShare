a0095ca3a84c24dbc2831d7adc86800828fe880d
msuzun <sevki_uzun53@hotmail.com>
2025-02-18 22:20:10 +0300

    Entities creation

---
+++ ClipShare.Core/Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ClipShare.Core.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+
+        //Navigations
+        public Channel Channel { get; set; }
+    }
+}
